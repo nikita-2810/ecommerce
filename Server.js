6476402add8e2fed6dabc4ee3b1fe90352a91acd
nikita-2810 <nikitaanchatageri@gmail.com>
2025-03-19 16:07:04 +0530

    second change

--- Server.js
+++ Server.js
@@ -4,7 +4,16 @@ const connectDB = require('./Config/db');
 const router = require('./Routes/authRoutes');
 const cartRouter = require('./Routes/cartRoutes');
 const app= express();
-app.use(cors())
+app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Allows cookies and authentication headers if needed
+  }))
 app.use(express.json())
 app.use("/auth",router)
 app.use("/cart",cartRouter)
@@ -12,7 +21,7 @@ connectDB()
 app.get('/',(req,res)=>{
     res.send("hello world")
 })
-const port=5003
+const port=3000
 app.listen(port,()=>{
     console.log(`server is running on http://localhost:${port}`)
 })
